574f9e29f2580b0b762a59295350b185858d7a34
SonaliN84 <sonali.nilekar@gmail.com>
2024-02-05 10:22:04 +0530

    Admin can accept book issue request

--- routers/user_book.py
+++ routers/user_book.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter,Depends,Path
 from sqlalchemy.orm import Session
 from starlette import status
 from dependencies import get_db,get_current_user
-from services.user_book import UserBookService
+from services.user_book import UserBookService,DetailResponse
+
 
 router = APIRouter(
     tags=['user_book']
@@ -17,3 +18,9 @@ async def request_book(data :dict,user: dict = Depends(get_current_user),db: Ses
 async def pending_books(user: dict = Depends(get_current_user),db: Session = Depends(get_db)):
     user_book_service = UserBookService(db)
     return user_book_service.pending_books(user)
+
+@router.put("/accept-book/{id}", status_code=status.HTTP_200_OK,response_model=DetailResponse)
+async def accept_book_request(user: dict = Depends(get_current_user),db: Session = Depends(get_db),id: int = Path(gt=0)):
+    print(id)
+    user_book_service = UserBookService(db)
+    return user_book_service.accept_book_request(user,id)
